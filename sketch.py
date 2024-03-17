05e2de0ec26ee24da47f3a6ab5b9f8add2689c3c
memonisha <75033521+memonisha@users.noreply.github.com>
2024-03-17 18:48:37 +0530

    Add files via upload

--- sketch.py
+++ sketch.py
@@ -11,16 +11,36 @@ def setup():
 def draw():
   background('black')
   drawTickAxes()
-  fill('white')
   noStroke()
+  textSize(25)
+  fill('plum')
   a.draw()
+  text(f"({a.x},{a.y})",width-150,height-50)
+  fill('lightblue')
   b.draw()
+  text(f"({b.x},{b.y})",width-150,height-100)
+  fill('teal')
   c.draw()
+  text(f"({c.x},{c.y})",width-150,height-150)
+
+  #(200,300) plum
+  #(500,400) lightblue
+  #(200,300) teal
 
   stroke("plum")
-  strokeWeight(3)
+  strokeWeight(4)
   line(b.x,b.y,a.x,a.y)
   stroke("lightblue")
   line(b.x,b.y,c.x,c.y)
   stroke("teal")
   line(c.x,c.y,a.x,a.y)
+
+  distPlum=distances(a,b)
+  distLightblue=distances(b,c)
+  distTeal=distances(a,c)
+
+def distances(a,b):
+  d=round(sqrt((a.x-b.x)**2+(a.y-b.y)**2))
+  print(d)
+  return d
+  
